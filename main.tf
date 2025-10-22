22c27b9c95f812e36c4e69f7ec8061e416467c50
SawGweAw <saw.gweaw@gmail.com>
2025-10-22 20:42:36 +0800

    Fix: main.tf terraform version add

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+
+required_version = ">= 1.0.0" # Specify a suitable version constraint
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
     key    = "saw-s3-tf-ci.tfstate" #Change this
